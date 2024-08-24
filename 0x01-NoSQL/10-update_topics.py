f23bd376f19e4d8cb2820956f6728fcc93379335
mwangii <jlmwangi254@gmail.com>
2024-08-24 10:12:17 +0300

    update based on a name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''changes all topics of a school doc based on the name'''
+
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    '''updates topics of a school based on the name'''
+    update_topic = mongo_collection.update_one(
+            {"name": name},
+            {$set: {"topics": topics}}
+    )
+    return update_topic
