9f56e79c190dd59d7c059df5f8239cc85a2e1029
mwangii <jlmwangi254@gmail.com>
2024-08-28 21:41:05 +0300

    recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,7 @@
 
 import redis
 import uuid
+from typing import Callable, Optional, Union
 
 
 class Cache:
@@ -20,3 +21,24 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable[bytes], Union[str,
+            bytes, int, float]] = None) -> Optional[Union[str,
+                                                    bytes, int, float]]:
+        '''optional callable that converts data back to desired format'''
+        data = self._redis.get(key)  # get data based on the key
+        if data is not None:
+            if fn is not None:  # if a callable is provided
+                return fn(data)  # transform the data with it
+            return data.decode('utf-8')
+        return None
+
+    def get_str(self, key: str) -> Optional[str]:
+        '''automatically parameterizes Cache.get
+        with correct conversion function'''
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Optional[int]:
+        ''' automatically parameterizes Cache.get
+        with correct conversion function'''
+        return self.get(key, lambda x: int(x.decode('utf-8')))
